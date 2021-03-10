3affeeb468ed0b2911d6860fd314efe8b2dabc08
Leif Ryge <leif@synthesize.us>
2021-03-10 12:07:16 +0100

    templated tests
    most fail right now, but this shows how we can write tests once and run them for everything

--- test/test_csidh.py
+++ test/test_csidh.py
@@ -1,13 +1,24 @@
 from unittest import TestCase, main
 from sidh.csidh import CSIDH
 
-class TestCSIDH(TestCase):
-
-#    def setUp(self):
+class CSIDH_test_base(object):
 
     def test_genpubvalidate(self):
-        c = CSIDH('montgomery', 'p512', 'hvelu', 'wd1', False, 2)
+        c = CSIDH('montgomery', 'p512', self.formula, self.style, False, 2)
         sk = c.gae.random_key()
         pk = c.gae.pubkey(sk)
         self.assertTrue(c.curve.validate(pk))
 
+
+FORMULAS = ('tvelu', 'svelu', 'hvelu')
+STYLES = ('df', 'wd1', 'wd2')
+
+STYLES = ('wd1',) # only this passes right now
+
+for formula in FORMULAS:
+    for style in STYLES:
+        class cls(CSIDH_test_base, TestCase):
+            formula = formula
+            style = style
+        globals()['csidh_'+formula+'_'+style] = cls
+del cls
