92b06156060f2c31c1fe0090ba88bab017b312dd
JJChiDguez <chidoys@gmail.com>
2021-03-17 13:13:49 +0200

    csidh/print_parameters.py is now working with the new moficiations

--- sidh/csidh/print_parameters.py
+++ sidh/csidh/print_parameters.py
@@ -1,8 +1,7 @@
 import click
 import numpy
 
-from sidh.common import attrdict
-from sidh.fp import printl
+from sidh.common import attrdict, printl
 from sidh.constants import strategy_data
 from sympy import floor, sqrt
 from math import pi
@@ -13,32 +12,31 @@ from math import pi
 def csidh_parameters(ctx):
     algo = ctx.meta['sidh.kwargs']['algo']
     setting = ctx.meta['sidh.kwargs']
-    A = algo.curve.A
-    global_L = L = algo.params.L
+    L = algo.params.L
     n = algo.params.n
     m = algo.params.m
-    delta = algo.params.delta
-    full_torsion_points = algo.curve.full_torsion_points
+    generators = algo.curve.generators
     set_parameters_velu = algo.formula.set_parameters_velu
-    set_zero_ops = algo.fp.set_zero_ops
-    get_ops = algo.fp.get_ops
-    KPs = algo.formula.KPs
-    xISOG = algo.formula.xISOG
-    xEVAL = algo.formula.xEVAL
+    init_runtime = algo.field.init_runtime
+    kps = algo.formula.kps
+    xisog = algo.formula.xisog
+    xeval = algo.formula.xeval
     sJ_list = algo.formula.sJ_list
-    xMUL = algo.curve.xMUL
+    xmul = algo.curve.xmul
+    HYBRID_BOUND = algo.formula.HYBRID_BOUND
 
+    A = [algo.curve.field(2), algo.curve.field(4)]
     if True:
 
         # T_p belongs to E[pi - 1]
         # T_m belongs to E[pi + 1]
-        T_p, T_m = full_torsion_points(A)
+        T_p, T_m = generators(A)
 
     else:
 
         # T_m belongs to E[pi - 1]
         # T_p belongs to E[pi + 1]
-        T_m, T_p = full_torsion_points(A)
+        T_m, T_p = generators(A)
 
     assert len(L) == n
     parameters = dict()
@@ -49,61 +47,68 @@ def csidh_parameters(ctx):
         Tp = list(T_p)
         for i in range(0, n, 1):
             if i != idx:
-                Tp = xMUL(Tp, A, i)
+                Tp = xmul(Tp, A, i)
 
         # -------------------------------------------------------------
         # Parameters sJ and sI correspond with the parameters b and b' from example 4.12 of https://eprint.iacr.org/2020/341
-        # These paramters are required in KPs, xISOG, and xEVAL
-        if global_L[idx] == 3:
+        # These paramters are required in kps, xisog, and xeval
+        if L[idx] == 3:
             b = 0
             c = 0
         else:
-            b = int(floor(sqrt(global_L[idx] - 1) / 2.0))
-            c = int(floor((global_L[idx] - 1.0) / (4.0 * b)))
+            b = int(floor(sqrt(L[idx] - 1) / 2.0))
+            c = int(floor((L[idx] - 1.0) / (4.0 * b)))
 
         b += 1
         parameters[str(idx)] = []
         for j in range(0, int(floor(sqrt(pi * (b - 1)) / 1.0)), 1):
 
             b -= 1
-            c = int(floor((global_L[idx] - 1.0) / (4.0 * b)))
+            c = int(floor((L[idx] - 1.0) / (4.0 * b)))
             set_parameters_velu(b, c, idx)
 
             total_cost = [0, 0, 0]
             # -------------------------------------------------------------
-            # KPs procedure
-            set_zero_ops()
-            KPs(Tp, A, idx)
-            t = get_ops()
-            total_cost[0] += t[0]
-            total_cost[1] += t[1]
-            total_cost[2] += t[2]
+            # kps procedure
+            init_runtime()
+            kps(Tp, A, idx)
+            
+            total_cost[0] += algo.field.fpmul
+            total_cost[1] += algo.field.fpsqr
+            total_cost[2] += algo.field.fpadd
 
             # -------------------------------------------------------------
-            # xISOG
-            set_zero_ops()
-            B = xISOG(A, idx)
-            t = get_ops()
-            total_cost[0] += t[0]
-            total_cost[1] += t[1]
-            total_cost[2] += t[2]
+            # xisog
+            init_runtime()
+            B = xisog(A, idx)
+            
+            total_cost[0] += algo.field.fpmul
+            total_cost[1] += algo.field.fpsqr
+            total_cost[2] += algo.field.fpadd
 
             # -------------------------------------------------------------
-            # xEVAL bench
-            set_zero_ops()
-            Tm = xEVAL(T_m, A)
-
-            t = get_ops()
-            total_cost[0] += t[0]
-            total_cost[1] += t[1]
-            total_cost[2] += t[2]
+            # xeval bench
+            init_runtime()
+            if L[idx] <= HYBRID_BOUND:
+                Tm = xeval(T_m, idx)
+            else:
+                Tm = xeval(T_m, A)
+            
+            total_cost[0] += algo.field.fpmul
+            total_cost[1] += algo.field.fpsqr
+            total_cost[2] += algo.field.fpadd
 
             # assert(validate(B))
             parameters[str(idx)].append(
-                (b, c, t, total_cost[0] + total_cost[1])
+                (
+                    b,
+                    c,
+                    [algo.field.fpmul, algo.field.fpsqr, algo.field.fpadd],
+                    total_cost[0] + total_cost[1]
+                )
             )
 
-        if global_L[idx] == 3:
+        if L[idx] == 3:
             parameters[str(idx)] = (0, 0, None, None)
         else:
             parameters[str(idx)] = min(
